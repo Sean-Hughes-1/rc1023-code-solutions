08d93be4465ad907c81b4ac112259e4a9ed5fb0e
Sean Hughes <coronapilot69@gmail.com>
2023-10-23 18:28:30 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,13 @@
+const fullName = 'Sean Hughes';
+const isCool = true;
+const totalPets = 4;
+const unknown = null;
+let never;
+
+console.log('Full name: ', fullName);
+console.log('isCool: ', isCool);
+console.log('Total Pets: ', totalPets);
+console.log('Unkown: ', unknown);
+console.log('Type of Unknown: ', typeof unknown);
+console.log('never: ', never);
+console.log('Typeof never: ', typeof never);
