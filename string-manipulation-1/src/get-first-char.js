43ea5db8c4b6682f6c6cfec5385e7c75a1394bd7
Sean Hughes <coronapilot69@gmail.com>
2023-10-26 20:55:25 +0000

    Added psuedo code to the functions

--- string-manipulation-1/src/get-first-char.js
+++ string-manipulation-1/src/get-first-char.js
@@ -1,4 +1,6 @@
 /* exported getFirstChar */
+/* Create a function that returns the first character of a string
+return the first character of the given string using the charAt method */
 function getFirstChar(string) {
   return string.charAt(0);
 }
