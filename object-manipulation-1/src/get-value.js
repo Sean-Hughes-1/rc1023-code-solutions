f1dc57c9a7871d9cb799f0e759eec4d266afee16
Sean Hughes <coronapilot69@gmail.com>
2023-10-26 20:41:47 +0000

    Added psuedo code to functions

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,4 +1,6 @@
 /* exported getValue */
+/* Create a function that accepts two arguments, an object and a key
+in that function, return the key of the object */
 function getValue(object, key) {
   return object[key];
 }
