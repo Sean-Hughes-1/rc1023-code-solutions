167d2197686f82a0a98c2f54b52895d510e7ae7b
Sean Hughes <coronapilot69@gmail.com>
2023-10-26 21:05:00 +0000

    Added psuedo code to the functions

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,4 +1,7 @@
 /* exported last */
+/** Create a function that accepts an array as its argument and returns the last element of that array
+ * call the length property of the array and subtract by one to get the last index of the array and return the array at that index
+ */
 function last(array) {
   return array[array.length - 1];
 }
