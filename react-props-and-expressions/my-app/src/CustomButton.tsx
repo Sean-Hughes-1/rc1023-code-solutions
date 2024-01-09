133eeca93e08a4be4faaff02dfedb480eaba6ac6
Sean Hughes <coronapilot69@gmail.com>
2024-01-09 20:08:15 +0000

    Completed react props and expressions

---
+++ react-props-and-expressions/my-app/src/CustomButton.tsx
@@ -0,0 +1,8 @@
+type Props = {
+  text: string;
+  color: string;
+};
+
+export default function CustomButton({ text, color }: Props) {
+  return <button style={{ backgroundColor: color }}>{text}</button>;
+}
